09a14f009511ff4e4647e97cbef6bcc7a0c6001e
linda-20 <lindaaron40@gmail.com>
2023-12-17 12:37:23 +0300

    Determine the marks based on the given ranges

--- grade.js
+++ grade.js
@@ -16,3 +16,16 @@ marks = Number(marks);
 if (isNaN(marks) || marks < 0 || marks > 100) {
   console.log("Invalid input! Please enter a number between 0 and 100.");
 } else {
+      // Determine the marks based on the given ranges
+  var grade;
+  if (marks > 79) {
+    grade = 'A';
+  } else if (marks >= 60 && marks <= 79) {
+    grade = 'B';
+  } else if (marks >= 50 && marks <= 59) {
+    grade = 'C';
+  } else if (marks >= 40 && marks <= 49) {
+    grade = 'D';
+  } else {
+    grade = 'E';
+  }
