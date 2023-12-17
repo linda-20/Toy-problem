acf6ae20c4121c031091ab4e12120e2f97097b0f
linda-20 <lindaaron40@gmail.com>
2023-12-17 13:10:27 +0300

    Final commit for speed dedector

--- speed.js
+++ speed.js
@@ -13,3 +13,18 @@ let speed = 100;
 // Define the speed limit and the demerit point threshold
 let speedLimit = 70;
 let demeritPointThreshold = 12;
+
+// calculate the demerit point based on the car speed
+let demeritPoints = 0;
+
+if (speed < speedLimit) {
+    console.log("Ok");
+  } else {
+    demeritPoints = Math.floor((speed - speedLimit) / 5);
+  
+    if (demeritPoints > demeritPointThreshold) {
+      console.log("License suspended");
+    } else {
+      console.log("Points: " + demeritPoints);
+    }
+  }
