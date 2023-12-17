bfcfd91e2efb09e526b8aeb5eb13d48b6bb9198b
linda-20 <lindaaron40@gmail.com>
2023-12-17 12:35:08 +0300

    check if the input is a valid number between 0 and 100

--- grade.js
+++ grade.js
@@ -5,5 +5,14 @@
 // D - 40 to 49, 
 // E - less 40.
 
-var marks = 50;
 
+// Prompt the user to input student marks
+var marks = 55;
+
+
+marks = Number(marks);
+
+// Check if the input is a valid number between 0 and 100
+if (isNaN(marks) || marks < 0 || marks > 100) {
+  console.log("Invalid input! Please enter a number between 0 and 100.");
+} else {
